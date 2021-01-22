61ca70ef77f0e1fcb00a6b850f02018e704b123a
Andrius Solopovas <andrius.solopovas@gmail.com>
2021-01-22 13:31:46 +0000

    save

--- mouse-acceleration.sh
+++ mouse-acceleration.sh
@@ -4,6 +4,6 @@ sudo bash -c 'cat <<EOT > /etc/X11/xorg.conf.d/99-libinput-mouse.conf
 Section "InputClass"
   Identifier "Mouse"
   MatchIsPointer "yes"
-  Option "AccelSpeed" "-1"
+  Option "AccelSpeed" "1"
 EndSection
 EOT'
